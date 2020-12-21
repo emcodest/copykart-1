bc12a976823ed182be7380926a70ada665824dd1
mpiranha <chukwubuezeokeke@gmail.com>
2020-12-21 21:43:17 +0100

    dashbooard and create

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+(function () {
+
+
+    $('.workspace-display').on('click', function () {
+        if ($('.profile-option').hasClass('show')) {
+            $('.profile-option').removeClass('show')
+        }
+        $('.workspace-options').toggleClass('show');
+    });
+
+    $('.create-btn').on('click', function () {
+        $('.create-options').toggleClass('show');
+    });
+
+    $('.profile-btn').on('click', function () {
+        if ($('.workspace-options').hasClass('show')) {
+            $('.workspace-options').removeClass('show')
+        }
+        $('.profile-option').toggleClass('show');
+    });
+
+    $('.campaign-box').each(function () {
+        $(this).on('click', function () {
+            removeActive($('.campaign-box'));
+            $(this).addClass('active');
+        });
+
+    });
+
+    function removeActive(elem) {
+        for (var i = 0; i < elem.length; i++) {
+            if (elem.hasClass('active')) {
+                elem.removeClass('active');
+            }
+        }
+    }
+})();
