ffbb6a29c7651ce8bae4f288267826956e11a3f3
mpiranha <chukwubuezeokeke@gmail.com>
2020-12-27 23:33:01 +0100

    facebook ad copy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,32 @@
 (function () {
 
+    const editor = new EditorJS({
+        holder: 'editorjs',
+        /** 
+         * Available Tools list. 
+         * Pass Tool's class or Settings object for each Tool you want to use 
+         */
+        tools: {
+            header: Header,
+            delimiter: Delimiter,
+            embed: Embed,
+            image: SimpleImage,
+            paragraph: {
+                class: Paragraph,
+                inlineToolbar: true,
+            },
+            image: {
+                class: ImageTool,
+                config: {
+                    endpoints: {
+                        byFile: 'http://localhost:8008/uploadFile', // Your backend file uploader endpoint
+                        byUrl: 'http://localhost:8008/fetchUrl', // Your endpoint that provides uploading by Url
+                    }
+                }
+            }
+        }
+    });
+
 
     $('.workspace-display').on('click', function () {
         if ($('.profile-option').hasClass('show')) {
@@ -12,11 +39,26 @@
         $('.create-options').toggleClass('show');
     });
 
+    $('#love-work').on('click', function (e) {
+        e.preventDefault();
+        $('#love-work span').toggleClass('loved');
+    });
+
     $('.profile-btn').on('click', function () {
         if ($('.workspace-options').hasClass('show')) {
             $('.workspace-options').removeClass('show')
         }
         $('.profile-option').toggleClass('show');
+    });
+
+    $('.floating-lock').on('click', function () {
+        if ($('#locker').hasClass('flaticon-lock')) {
+            $('#locker').removeClass('flaticon-lock');
+            $('#locker').addClass('flaticon-open-padlock-silhouette');
+        } else {
+            $('#locker').removeClass('flaticon-open-padlock-silhouette');
+            $('#locker').addClass('flaticon-lock');
+        }
     });
 
     $('.campaign-box').each(function () {
